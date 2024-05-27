a814ff56c74a922931e0351f139b0fc9e4edbeb1
Yoann SIMON <simon.y@sfeir.com>
2024-05-27 16:15:08 +0200

    Update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import {secondStepConfigurationGuard, thirdStepConfigurationGuard} from "./core/guard/tesla.guard";
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'first-step', pathMatch: 'full' },
+  { path: '', redirectTo: '/first-step', pathMatch: 'full' },
   { path: 'first-step', loadComponent: () => import('./features/first-step/first-step.component').then(m => m.FirstStepComponent) },
   { path: 'second-step', loadComponent: () => import('./features/second-step/second-step.component').then(m => m.SecondStepComponent), canActivate: [secondStepConfigurationGuard] },
   { path: 'third-step', loadComponent: () => import('./features/third-step/third-step.component').then(m => m.ThirdStepComponent), canActivate: [thirdStepConfigurationGuard] },
+  { path: '**', redirectTo: '/first-step' },
 ];
